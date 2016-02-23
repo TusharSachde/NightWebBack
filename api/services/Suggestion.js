fa241cd61224d4a187289c6c35e1a1d15c2b09a5
TusharSachde <tushar@wohlig.com>
2016-02-23 15:54:20 +0530

    API AS WELL.

--- api/services/Suggestion.js
+++ api/services/Suggestion.js
@@ -1,15 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-id
-user
-name
-email
-contact
-subject
-query
-
-
 var schema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
@@ -23,6 +14,44 @@ var schema = new Schema({
 });
 
 module.exports = mongoose.model('Suggestion', schema);
-var models = {};
+var models = {
+  saveData: function(data, callback) {
+    var project = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, callback);
+    } else {
+      this.save(function(err, data) {
+        if (err) {
+          callback(err, false);
+        } else {
+          callback(null, data);
+        }
+      });
+    }
+  },
+  deleteData: function(data, callback) {
+    this.findOneAndRemove({
+      _id: data._id
+    }, function(err, data) {
+
+      if (err) {
+        callback(err, false);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  getAll: function(data, callback) {
+    this.find().exec(callback);
+  },
+  getOne: function(data, callback) {
+    this.findOne({
+      "_id": data._id
+    }).exec(callback);
+  }
+
+};
 
 module.exports = _.assign(module.exports, models);
--- api/services/UserPlan.js
+++ api/services/UserPlan.js
@@ -15,11 +15,49 @@ var schema = new Schema({
   startTime: Date,
   endTime: Date,
   url: String,
-  json: Schema.Types.Mixed
+  json: Schema.Types.Mixed,
   modificationTime: Date
 });
 
 module.exports = mongoose.model('UserNotification', schema);
-var models = {};
+var models = {
+  saveData: function(data, callback) {
+    var project = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, callback);
+    } else {
+      this.save(function(err, data) {
+        if (err) {
+          callback(err, false);
+        } else {
+          callback(null, data);
+        }
+      });
+    }
+  },
+  deleteData: function(data, callback) {
+    this.findOneAndRemove({
+      _id: data._id
+    }, function(err, data) {
+
+      if (err) {
+        callback(err, false);
+      } else {
+        callback(null, data);
+      }
+    });
+  },
+  getAll: function(data, callback) {
+    this.find().exec(callback);
+  },
+  getOne: function(data, callback) {
+    this.findOne({
+      "_id": data._id
+    }).exec(callback);
+  }
+
+};
 
 module.exports = _.assign(module.exports, models);
